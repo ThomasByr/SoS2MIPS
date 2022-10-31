b88f23f9ed10a5b40f98ff78d260895240c49318
ThomasByr <tbouyer2000@gmail.com>
2022-10-31 17:21:19 +0100

    launch App & qtspim [fork]

---
+++ inc/app.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "io.h"
+
+/**
+ * @brief Runs the application with the given command line arguments
+ *
+ * @param args command line arguments structure
+ */
+int run_app(const struct cmd_args *args);
---
+++ src/app.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdnoreturn.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "app.h"
+#include "threadpool.h"
+
+#include "lib.h"
+
+void discard_file(void *filename) {
+  char *f = (char *)filename;
+  CHK(remove(f));
+}
+
+void launch_qtspim(void *filename) {
+  int status;
+  switch (fork()) {
+  case -1:
+    panic("fork failed");
+  case 0:
+    execlp("qtspim", "qtspim", "-file", (char *)filename, NULL);
+    panic("execlp failed");
+  }
+  CHK(wait(&status));
+  if (WIFEXITED(status)) {
+    if (WEXITSTATUS(status) != EXIT_SUCCESS) {
+      panic("qtspim exited with status %d", WEXITSTATUS(status));
+    }
+  } else if (WIFSIGNALED(status)) {
+    panic("qtspim terminated by signal %d", WTERMSIG(status));
+  } else {
+    panic("qtspim terminated abnormally");
+  }
+}
+
+int run_app(const struct cmd_args *args) {
+#define THREAD 32
+#define QUEUE 256
+
+  int status = EXIT_SUCCESS;
+
+  threadpool_t pool = threadpool_create(THREAD, QUEUE, 0);
+  if (pool == NULL) panic("failed to create threadpool");
+
+  /*
+  extern FILE *yyin, *yyout;
+  extern int yyparse(void), yydebug;
+
+  // open input and output files
+  yyin = fopen(args->filename, "r");
+  if (yyin == NULL) panic("failed to open input file");
+  yyout = fopen(args->output, "w");
+  if (yyout == NULL) panic("failed to open output file");
+
+  // launch yyparse
+  yydebug = 0;
+  if (yyparse() != 0) {
+    status = EXIT_FAILURE;
+  }
+  */
+
+  threadpool_add(pool, launch_qtspim, args->output, 0); // launch qtspim
+  threadpool_destroy(pool, 1);                          // wait
+
+  /*
+  if (args->dispose_on_exit) {
+    discard_file(args->output);
+  }
+
+  CHK(fclose(yyin));
+  CHK(fclose(yyout));
+  */
+
+  return status;
+}
