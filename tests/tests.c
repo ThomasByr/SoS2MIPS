47c4d21153ee9f847ea2018ff3d88d122a1c09a8
ThomasByr <tbouyer2000@gmail.com>
2022-12-17 16:09:17 +0100

    thread safety scary hehe 👻

--- tests/tests.c
+++ tests/tests.c
@@ -2,11 +2,14 @@
 
 #include "dict_test.h"
 #include "threadpool_test.h"
+#include "vec_test.h"
 
 unsigned long _no_asserts = 0;
 
 int main(void) {
   thpool_test();
   dict_test();
+  vec_test();
+
   return EXIT_SUCCESS;
 }
---
+++ tests/vec_test.h
@@ -0,0 +1,101 @@
+#include <pthread.h>
+#include <stdlib.h>
+
+#include "m.h"
+
+#include "vec.h"
+
+void access_vec(void *arg) { (void)arg; }
+
+void vec_test_0(void) {
+  vec_t v = vec_new();
+  vec_push(v, (void *)1);
+  vec_push(v, (void *)2);
+  vec_push(v, (void *)3);
+  vec_push(v, (void *)4);
+
+  assert_eq(vec_size(v), 4);
+  assert_eq(vec_get(v, 0), (void *)1);
+  assert_eq(vec_get(v, 1), (void *)2);
+  assert_eq(vec_get(v, 2), (void *)3);
+  assert_eq(vec_get(v, 3), (void *)4);
+
+  vec_free(v);
+}
+
+void vec_test_1(void) {
+  size_t n = 1000;
+  vec_t v = vec_new(n);
+  assert_eq(vec_size(v), 0);
+  assert_eq(vec_capacity(v), n);
+
+  for (size_t i = 0; i < n; i++) {
+    vec_push(v, (void *)i);
+  }
+
+  assert_eq(vec_size(v), n);
+  assert_geq(vec_capacity(v), n);
+
+  for (size_t i = 0; i < n; i++) {
+    assert_eq(vec_get(v, i), (void *)i);
+  }
+
+  vec_free(v);
+}
+
+void vec_test_2(void) {
+  size_t n = 1000;
+  vec_t v = vec_new(n);
+  assert_eq(vec_size(v), 0);
+  assert_eq(vec_capacity(v), n);
+
+  for (size_t i = 0; i < n; i++) {
+    // here we add +1 to i to avoid 0 (NULL) values
+    vec_push(v, (void *)(i + 1));
+  }
+
+  assert_eq(vec_size(v), n);
+  assert_geq(vec_capacity(v), n);
+
+  void *data;
+  vec_foreach(v, i, data) {
+    // as we check `data = vec_get(v, i)` against NULL
+    // we need to not get 0 as the first value
+    assert_eq(data, (void *)(i + 1));
+  }
+
+  vec_free(v);
+}
+
+void vec_test_3(void) {
+  vec_t v = vec_from((void *)1, (void *)2, (void *)3, (void *)4, NULL);
+
+  assert_eq(vec_size(v), 4);
+  assert_geq(vec_capacity(v), 4);
+
+  assert_eq(vec_get(v, 0), (void *)1);
+  assert_eq(vec_get(v, 1), (void *)2);
+  assert_eq(vec_get(v, 2), (void *)3);
+  assert_eq(vec_get(v, 3), (void *)4);
+
+  vec_free(v);
+}
+
+void vec_test_4(void) {
+  size_t n = 1000;
+  vec_t v = vec_new(n);
+
+  for (size_t i = 0; i < n; i++) {
+    vec_push(v, (void *)i);
+  }
+
+  // todo: check for thread safety
+}
+
+void vec_test(void) {
+  test_case(vec_test_0);
+  test_case(vec_test_1);
+  test_case(vec_test_2);
+  test_case(vec_test_3);
+  // test_case(vec_test_4);
+}
