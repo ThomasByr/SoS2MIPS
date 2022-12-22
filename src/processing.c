39560ba0f4c8b5c13e5d6783d3bcf4487327a8c1
ThomasByr <tbouyer2000@gmail.com>
2022-12-22 18:30:34 +0100

    lol, it compiles

--- src/processing.c
+++ src/processing.c
@@ -509,9 +509,7 @@ int fill_id_types(struct pnode *node) {
   case ROOT: {
     for (child = node->left_child; child != NULL;
          child = child->right_sibling) {
-      if (fill_id_types(child) != 0) {
-        return 1;
-      }
+      if (fill_id_types(child) != 0) return 1;
     }
 
     return 0;
@@ -593,9 +591,7 @@ int fill_id_types(struct pnode *node) {
     // recurse on children
     for (child = node->left_child; child != NULL;
          child = child->right_sibling) {
-      if (fill_id_types(child) != 0) {
-        return 1;
-      }
+      if (fill_id_types(child) != 0) return 1;
     }
 
     // exit the SEQ's scope (but only if it entered one at the beginning)
@@ -639,4 +635,609 @@ int fill_id_types(struct pnode *node) {
   default: // this is to make -Wall happy
     return 0;
   }
+}
+
+int type_check(struct pnode *node) {
+  struct pnode *child;
+  for (child = node->left_child; child != NULL; child = child->right_sibling) {
+    if (type_check(child) != 0) return 1;
+  }
+
+  node->data_type = no_type;
+  node->return_type = no_type;
+
+  enum vartype sub_type;
+  enum vartype ltype;
+  enum vartype rtype;
+  enum vartype dtype;
+
+  enum vartype decl_return_type, body_return_type;
+  struct pnode *seq_node;
+
+  struct symnode *func_sym_node;
+  int i;
+  struct pnode *actual_param_node;
+
+  enum vartype actual_type;
+  enum vartype formal_type;
+
+  int shouldWiden;
+
+  switch (node->node_type) {
+  case ROOT:
+    return 0;
+  case ID:
+    node->data_type = node->value.symnode->var_type;
+    return 0;
+  case INT_TYPE:
+    return 0;
+  case DBL_TYPE:
+    return 0;
+  case VOID_TYPE:
+    return 0;
+
+  case ARRAY_SUB: {
+    if (node->left_child == NULL) {
+      mark_error(node->lineno, "Array subscript expression is missing");
+      return 1;
+    }
+    if (node->left_child->right_sibling == NULL) {
+      mark_error(node->lineno, "Array subscript expression is missing");
+      return 1;
+    }
+    sub_type = node->left_child->right_sibling->data_type;
+    if (sub_type != inttype) {
+      mark_error(node->lineno, "Array index is not an int");
+      return 1;
+    } else if (is_array_node(node->left_child->right_sibling)) {
+      mark_error(node->lineno,
+                 "Array subscript expression cannot be an array identifier");
+      return 1;
+    } else if (node->left_child->value.symnode->node_type != array_node) {
+      mark_error(node->lineno, "Subscripted variable is not an array");
+      return 1;
+    }
+
+    node->data_type = node->left_child->value.symnode->var_type;
+
+    return 0;
+  }
+
+  case ARRAY_NONSUB: {
+    if (node->left_child == NULL) {
+      mark_error(node->lineno, "Array subscript expression is missing");
+      return 1;
+    }
+    if (node->left_child->right_sibling == NULL) {
+      mark_error(node->lineno, "Array subscript expression is missing");
+      return 1;
+    }
+    node->data_type = node->left_child->right_sibling->value.symnode->var_type;
+    return 0;
+  }
+
+  case OP_ASSIGN: {
+    if (node->left_child == NULL) {
+      mark_error(node->lineno, "Left-hand side of assignment is missing");
+      return 1;
+    }
+    if (node->left_child->right_sibling == NULL) {
+      mark_error(node->lineno, "Right-hand side of assignment is missing");
+      return 1;
+    }
+    if (is_array_node(node->left_child)) {
+      mark_error(node->lineno, "Cannot assign to an array pointer");
+      return 1;
+    } else if (is_array_node(node->left_child->right_sibling)) {
+      mark_error(node->lineno, "Cannot assign an array pointer to a variable");
+      return 1;
+    }
+
+    ltype = node->left_child->data_type;
+    rtype = node->left_child->right_sibling->data_type;
+
+    // widen if assigning int to double
+    shouldWiden = (ltype == doubletype) && (rtype == inttype);
+    if (rtype != ltype && !shouldWiden) {
+      mark_error(node->lineno, "Cannot assign a double to an int");
+      return 1;
+    }
+    node->data_type = ltype;
+    return 0;
+  }
+
+  case OP_ADD:
+    return standard_binary_op_typecheck_widening(node);
+  case OP_SUB:
+    return standard_binary_op_typecheck_widening(node);
+  case OP_MULT:
+    return standard_binary_op_typecheck_widening(node);
+  case OP_DIV:
+    return standard_binary_op_typecheck_widening(node);
+
+  case OP_MOD: {
+    if (node->left_child == NULL) {
+      mark_error(node->lineno, "Left-hand side of mod is missing");
+      return 1;
+    }
+    if (node->left_child->right_sibling == NULL) {
+      mark_error(node->lineno, "Right-hand side of mod is missing");
+      return 1;
+    }
+
+    ltype = node->left_child->data_type;
+    rtype = node->left_child->right_sibling->data_type;
+
+    if (ltype != inttype || rtype != inttype) {
+      mark_error(node->lineno, "An operand of mod is not of type int");
+      return 1;
+    } else if (is_array_node(node->left_child) ||
+               is_array_node(node->left_child->right_sibling)) {
+      mark_error(node->lineno,
+                 "The mod operator cannot act on an array identifier operand");
+      return 1;
+    }
+
+    node->data_type = inttype;
+    return 0;
+  }
+
+  case OP_LT:
+    return standard_binary_op_typecheck_int(node);
+  case OP_LEQ:
+    return standard_binary_op_typecheck_int(node);
+  case OP_GT:
+    return standard_binary_op_typecheck_int(node);
+  case OP_GEQ:
+    return standard_binary_op_typecheck_int(node);
+  case OP_EQ:
+    return standard_binary_op_typecheck_int(node);
+  case OP_NEQ:
+    return standard_binary_op_typecheck_int(node);
+
+  case OP_AND: {
+    if (node->left_child == NULL) {
+      mark_error(node->lineno, "Left-hand side of && is missing");
+      return 1;
+    }
+    if (node->left_child->right_sibling == NULL) {
+      mark_error(node->lineno, "Right-hand side of && is missing");
+      return 1;
+    }
+
+    ltype = node->left_child->data_type;
+    rtype = node->left_child->right_sibling->data_type;
+
+    if (ltype != inttype || rtype != inttype) {
+      mark_error(node->lineno, "An operand of && is not an int");
+      return 1;
+    } else if (is_array_node(node->left_child) ||
+               is_array_node(node->left_child->right_sibling)) {
+      mark_error(node->lineno,
+                 "The && operator cannot act on an array identifier operand");
+      return 1;
+    }
+
+    node->data_type = inttype;
+    return 0;
+  }
+
+  case OP_OR: {
+    if (node->left_child == NULL) {
+      mark_error(node->lineno, "Left-hand side of || is missing");
+      return 1;
+    }
+    if (node->left_child->right_sibling == NULL) {
+      mark_error(node->lineno, "Right-hand side of || is missing");
+      return 1;
+    }
+
+    ltype = node->left_child->data_type;
+    rtype = node->left_child->right_sibling->data_type;
+
+    if (ltype != inttype || rtype != inttype) {
+      mark_error(node->lineno, "An operand of || is not an int");
+      return 1;
+    } else if (is_array_node(node->left_child) ||
+               is_array_node(node->left_child->right_sibling)) {
+      mark_error(node->lineno,
+                 "The || operator cannot act on an array identifier operand");
+      return 1;
+    }
+
+    node->data_type = inttype;
+    return 0;
+  }
+
+  case OP_BANG: {
+    if (node->left_child == NULL) {
+      mark_error(node->lineno, "Operand of ! is missing");
+      return 1;
+    }
+
+    if (node->left_child->data_type != inttype) {
+      mark_error(node->lineno, "The operand of ! is not an int");
+      return 1;
+    } else if (is_array_node(node->left_child)) {
+      mark_error(node->lineno,
+                 "The ! operator cannot act on an array identifier operand");
+      return 1;
+    }
+
+    node->data_type = inttype;
+    return 0;
+  }
+
+  case OP_NEG: {
+    if (node->left_child == NULL) {
+      mark_error(node->lineno, "Operand of - (unary minus) is missing");
+      return 1;
+    }
+
+    dtype = node->left_child->data_type;
+
+    if (dtype != inttype && dtype != doubletype) {
+      mark_error(node->lineno,
+                 "The operand of - (unary minus) is not an int or double");
+      return 1;
+    } else if (is_array_node(node->left_child)) {
+      mark_error(
+          node->lineno,
+          "The negation operator cannot act on an array identifier operand");
+      return 1;
+    }
+
+    node->data_type = dtype;
+    return 0;
+  }
+
+  case OP_INC: {
+    if (node->left_child == NULL) {
+      mark_error(node->lineno, "Operand of ++ is missing");
+      return 1;
+    }
+
+    dtype = node->left_child->data_type;
+
+    if (dtype != inttype) {
+      mark_error(node->lineno, "The operand of ++ is not an int");
+      return 1;
+    } else if (is_array_node(node->left_child)) {
+      mark_error(node->lineno,
+                 "The ++ operator cannot act on an array pointer");
+      return 1;
+    }
+
+    node->data_type = inttype;
+    return 0;
+  }
+
+  case OP_DEC: {
+    if (node->left_child == NULL) {
+      mark_error(node->lineno, "Operand of -- is missing");
+      return 1;
+    }
+
+    dtype = node->left_child->data_type;
+
+    if (dtype != inttype) {
+      mark_error(node->lineno, "The operand of -- is not an int");
+      return 1;
+    } else if (is_array_node(node->left_child)) {
+      mark_error(node->lineno,
+                 "The -- operator cannot act on an array identifier operand");
+      return 1;
+    }
+
+    node->data_type = inttype;
+    return 0;
+  }
+
+  case FUNC_DECL: {
+    if (node->left_child == NULL) {
+      mark_error(node->lineno, "Function declaration is missing a name");
+      return 1;
+    }
+    if (node->left_child->right_sibling == NULL) {
+      mark_error(node->lineno, "Function declaration is missing a body");
+      return 1;
+    }
+
+    decl_return_type = node->left_child->right_sibling->value.symnode->var_type;
+
+    // clang-format off
+    for (seq_node = node->left_child; seq_node->right_sibling != NULL;
+         seq_node = seq_node->right_sibling); // bitchin'
+    // clang-format on
+
+    body_return_type = seq_node->return_type;
+
+    if (!((body_return_type == no_type && decl_return_type == voidtype) ||
+          (body_return_type == decl_return_type) ||
+          (body_return_type == inttype && decl_return_type == doubletype))) {
+      mark_error(
+          node->lineno,
+          "The return types of the function declaration and body do not match");
+      return 1;
+    }
+
+    node->return_type = decl_return_type;
+
+    return 0;
+  }
+
+  case VAR_DECL:
+    return 0;
+  case FORMAL_PARAM:
+    return 0;
+
+  case SEQ: {
+    for (child = node->left_child; child != NULL;
+         child = child->right_sibling) {
+      if (node->return_type == no_type) {
+        node->return_type = child->return_type;
+      } else if (node->return_type == inttype &&
+                 child->return_type == doubletype) { // Widen to doubletype
+        node->return_type = doubletype;
+      } else if (child->return_type != no_type &&
+                 node->return_type != child->return_type) {
+        mark_error(node->lineno,
+                   "The type of this return statement conflicts with that of a "
+                   "previous return statement in this function");
+        return 1;
+      }
+    }
+
+    return 0;
+  }
+
+  case IF_STMT:
+    if (node->left_child == NULL) {
+      mark_error(node->lineno, "if statement is missing a condition");
+      return 1;
+    }
+    if (node->left_child->right_sibling == NULL) {
+      mark_error(node->lineno, "if statement is missing a body");
+      return 1;
+    }
+
+    if (node->left_child->right_sibling->right_sibling == NULL) {
+      node->return_type = node->left_child->right_sibling->return_type;
+    } else {
+      if (node->left_child->right_sibling->return_type ==
+              node->left_child->right_sibling->right_sibling->return_type &&
+          node->left_child->right_sibling->return_type != no_type) {
+        node->return_type = node->left_child->right_sibling->return_type;
+      } else if (node->left_child->right_sibling->return_type == no_type &&
+                 node->left_child->right_sibling->right_sibling->return_type !=
+                     no_type) {
+        node->return_type =
+            node->left_child->right_sibling->right_sibling->return_type;
+      } else if (node->left_child->right_sibling->return_type != no_type &&
+                 node->left_child->right_sibling->right_sibling->return_type ==
+                     no_type) {
+        node->return_type = node->left_child->right_sibling->return_type;
+      } else {
+        node->return_type = no_type;
+      }
+    }
+    node->return_type = node->left_child->right_sibling->return_type;
+    return 0;
+  case WHILE_LOOP:
+    if (node->left_child == NULL) {
+      mark_error(node->lineno, "while loop is missing a condition");
+      return 1;
+    }
+    if (node->left_child->right_sibling == NULL) {
+      mark_error(node->lineno, "while loop is missing a body");
+      return 1;
+    }
+    node->return_type = node->left_child->right_sibling->return_type;
+    return 0;
+  case DO_WHILE_LOOP:
+    if (node->left_child == NULL) {
+      mark_error(node->lineno, "do-while loop is missing a condition");
+      return 1;
+    }
+    if (node->left_child->right_sibling == NULL) {
+      mark_error(node->lineno, "do-while loop is missing a body");
+      return 1;
+    }
+    node->return_type = node->left_child->return_type;
+    return 0;
+  case FOR_STMT:
+    if (node->left_child == NULL) {
+      mark_error(node->lineno, "for loop is missing an initialization");
+      return 1;
+    }
+    if (node->left_child->right_sibling == NULL) {
+      mark_error(node->lineno, "for loop is missing a condition");
+      return 1;
+    }
+    if (node->left_child->right_sibling->right_sibling == NULL) {
+      mark_error(node->lineno, "for loop is missing an increment");
+      return 1;
+    }
+    if (node->left_child->right_sibling->right_sibling->right_sibling == NULL) {
+      mark_error(node->lineno, "for loop is missing a body");
+      return 1;
+    }
+
+    node->return_type = node->left_child->right_sibling->right_sibling
+                            ->right_sibling->return_type;
+    return 0;
+
+  case RETURN_STMT: {
+    if (node->left_child != NULL) {
+      if (is_array_node(node->left_child)) {
+        mark_error(node->lineno, "Cannot return an array variable");
+        return 1;
+      } else {
+        node->return_type = node->left_child->data_type;
+      }
+    } else {
+      node->return_type = voidtype;
+    }
+
+    return 0;
+  }
+
+  case READ_STMT: {
+    if (is_array_node(node->left_child)) {
+      mark_error(node->lineno,
+                 "Cannot read into an array variable that is not subscripted");
+    }
+
+    return 0;
+  }
+
+  case PRINT_STMT: {
+    if (is_array_node(node->left_child)) {
+      mark_error(node->lineno,
+                 "Cannot print an array variable that is not subscripted");
+    }
+
+    return 0;
+  }
+
+  case STRING_LITERAL:
+    return 0;
+  case INT_LITERAL:
+    node->data_type = inttype;
+    return 0;
+  case DOUBLE_LITERAL:
+    node->data_type = doubletype;
+    return 0;
+
+  case FUNC_CALL: {
+    if (node->left_child == NULL) {
+      mark_error(node->lineno, "Function call is missing a name");
+      return 1;
+    }
+
+    // get function's sym_node
+    func_sym_node = node->left_child->value.symnode;
+
+    // set data_type equal to return type of function
+    node->data_type = func_sym_node->var_type;
+
+    // check that there are the correct number of actual parameters
+    for (actual_param_node = node->left_child->right_sibling, i = 0;
+         actual_param_node != NULL;
+         actual_param_node = actual_param_node->right_sibling, i++)
+      ;
+    if (i != func_sym_node->num_params) {
+      mark_error(node->lineno,
+                 "The number of actual parameters passed does not match the "
+                 "number of formal parameters in the function declaration");
+      return 1;
+    }
+
+    // check parameter types
+    for (actual_param_node = node->left_child->right_sibling, i = 0;
+         actual_param_node != NULL;
+         actual_param_node = actual_param_node->right_sibling, i++) {
+      // if the function wants an array pointer
+      if (func_sym_node->param_symnode_array[i]->node_type == array_node &&
+          (actual_param_node->node_type != ID ||
+           actual_param_node->value.symnode->node_type != array_node)) {
+        mark_error(node->lineno,
+                   "A formal parameter is an array pointer, but the "
+                   "corresponding actual parameter passed is not");
+        return 1;
+      }
+
+      // if the function doesn't want an array pointer but gets one
+      if (func_sym_node->param_symnode_array[i]->node_type != array_node &&
+          (actual_param_node->node_type == ID &&
+           actual_param_node->value.symnode->node_type == array_node)) {
+        mark_error(node->lineno,
+                   "A formal parameter is not an array pointer, but the "
+                   "corresponding actual parameter passed is");
+      }
+
+      actual_type = actual_param_node->data_type;
+      formal_type = func_sym_node->param_symnode_array[i]->var_type;
+
+      // error if you are expecting an int and get a double
+      if (formal_type == inttype && actual_type == doubletype) {
+        mark_error(node->lineno,
+                   "An actual parameter passed is a double, but the "
+                   "corresponding formal parameter is an int");
+        return 1;
+      } else if (actual_type ==
+                 voidtype) { // error if a parameter passed is of type void
+        mark_error(node->lineno, "You can't pass a parameter of type void");
+        return 1;
+      }
+    }
+
+    return 0;
+  }
+
+  case EMPTY_EXPR:
+    return 0;
+  }
+#ifdef DEBUG
+  pnode_display(node);
+#endif
+  return 0;
+}
+
+int standard_binary_op_typecheck_widening(struct pnode *node) {
+  if (node->left_child == NULL || node->left_child->right_sibling == NULL) {
+    mark_error(node->lineno, "Binary operator is missing an operand");
+    return 1;
+  }
+
+  enum vartype ltype = node->left_child->data_type;
+  enum vartype rtype = node->left_child->right_sibling->data_type;
+
+  if ((ltype != inttype && ltype != doubletype) ||
+      (rtype != inttype && rtype != doubletype)) {
+    mark_error(node->lineno, "An operand is not of type int or double");
+    return 1;
+  } else if (is_array_node(node->left_child) ||
+             is_array_node(node->left_child->right_sibling)) {
+    mark_error(node->lineno, "One of the operands is an array variable");
+    return 1;
+  } else if ((ltype == doubletype) || (rtype == doubletype)) {
+    node->data_type = doubletype;
+  } else {
+    node->data_type = inttype;
+  }
+
+  return 0;
+}
+
+int standard_binary_op_typecheck_int(struct pnode *node) {
+
+  if (node->left_child == NULL || node->left_child->right_sibling == NULL) {
+    mark_error(node->lineno, "Binary operator is missing an operand");
+    return 1;
+  }
+
+  enum vartype ltype = node->left_child->data_type;
+  enum vartype rtype = node->left_child->right_sibling->data_type;
+
+  if ((ltype != inttype && ltype != doubletype) ||
+      (rtype != inttype && rtype != doubletype)) {
+    mark_error(node->lineno, "An operand is not of type int or float");
+    return 1;
+  } else if (is_array_node(node->left_child) ||
+             is_array_node(node->left_child->right_sibling)) {
+    mark_error(node->lineno, "One of the operands is an array variable");
+    return 1;
+  }
+
+  node->data_type = inttype;
+
+  return 0;
+}
+
+int is_array_node(struct pnode *node) {
+  if (node == NULL)
+    return 0;
+
+  return node->node_type == ID && node->value.symnode->node_type == array_node;
 }
