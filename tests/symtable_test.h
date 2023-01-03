4d22b85e8189c989c6a7cdc284b2e8cf05a642f2
ThomasByr <tbouyer2000@gmail.com>
2023-01-03 13:41:02 +0100

    found symtable invalid read of size 8 👻

--- tests/symtable_test.h
+++ tests/symtable_test.h
@@ -47,9 +47,38 @@ void symtable_test_1(void) {
   assert_eq(level, 0);
   assert_neq(symtable_lookup(symtable, "f", &level), NULL);
   assert_eq(level, 0);
-  
+
   assert_eq(symtable_lookup(symtable, "c", &level), NULL);
   assert_eq(symtable_lookup(symtable, "d", &level), NULL);
+
+  symtable_free(symtable);
+}
+
+void symtable_test_2(void) {
+  struct symtable *symtable = symtable_new();
+
+  struct symnode *node_a = symtable_insert(symtable, "a");
+  node_a->node_type = val_node;
+  node_a->var_type = inttype;
+  node_a->mem_addr_type = global;
+  node_a->var_addr = 0;
+
+  symtable_enter_scope(symtable);
+
+  struct symnode *node_b = symtable_insert(symtable, "b");
+  node_b->node_type = val_node;
+  node_b->var_type = inttype;
+  node_b->mem_addr_type = off_fp;
+  node_b->var_addr = 0;
+  symtable_display(symtable); // to check if we have valid read/write
+
+  assert_eq(symtable_lookup(symtable, "a", NULL), node_a);
+  assert_eq(symtable_lookup(symtable, "b", NULL), node_b);
+
+  symtable_leave_scope(symtable);
+
+  assert_eq(symtable_lookup(symtable, "a", NULL), node_a);
+  assert_eq(symtable_lookup(symtable, "b", NULL), NULL);
 
   symtable_free(symtable);
 }
@@ -57,4 +86,5 @@ void symtable_test_1(void) {
 void symtable_test(void) {
   test_case(symtable_test_0);
   test_case(symtable_test_1);
+  test_case(symtable_test_2);
 }
