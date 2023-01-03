43a3a412e1908da1a628bb71c9beaf4de7d5b738
ThomasByr <tbouyer2000@gmail.com>
2023-01-03 13:53:19 +0100

    found symtable invalid read of size 8 👻

--- tests/tests.c
+++ tests/tests.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdbool.h>
 
 #include "dict_test.h"
 #include "symtable_test.h"
@@ -6,6 +7,10 @@
 #include "vec_test.h"
 
 unsigned long _no_asserts = 0;
+
+int opt_lvl = 0;
+bool verbose = false;
+char *output = NULL;
 
 int main(void) {
   thpool_test();
